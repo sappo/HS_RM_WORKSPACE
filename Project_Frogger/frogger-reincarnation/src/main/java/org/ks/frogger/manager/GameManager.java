df85f6c06321d3535bba224d9e5c2814617b69ef
kesa <kesa@PC-IKANO247.ikano.de>
2012-01-06 16:06:50 +0100

    Project Frogger - moved events from GameManager to TimeManager

--- Project_Frogger/frogger-reincarnation/src/main/java/org/ks/frogger/manager/GameManager.java
+++ Project_Frogger/frogger-reincarnation/src/main/java/org/ks/frogger/manager/GameManager.java
@@ -1,7 +1,6 @@
 package org.ks.frogger.manager;
 
 import java.awt.Dimension;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -10,7 +9,6 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Event;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
-import org.ks.frogger.ActionCommand;
 import org.ks.frogger.events.FroggerDeath;
 import org.ks.frogger.events.GameOver;
 import org.ks.frogger.events.LifeUpdate;
@@ -30,205 +28,190 @@ import org.ks.sf.shape.Rectangle;
  * @author Kevin Sapper
  */
 @ApplicationScoped
-public class GameManager implements KeyListener, ActionListener {
+public class GameManager implements KeyListener {
 
-  private static final int LEFT = 37;
+    private static final int LEFT = 37;
 
-  private static final int UP = 38;
+    private static final int UP = 38;
 
-  private static final int RIGHT = 39;
+    private static final int RIGHT = 39;
 
-  private static final int DOWN = 40;
+    private static final int DOWN = 40;
 
-  private static final Vector froggerStartPos = new Vector(250, 50);
+    private static final Vector froggerStartPos = new Vector(250, 50);
 
-  private static final Vector froggerSize = new Vector(30, 30);
+    private static final Vector froggerSize = new Vector(30, 30);
 
-  private long remainingLives = 3;
+    private long remainingLives = 3;
 
-  private long remainingFroggersToSave = 5;
+    private long remainingFroggersToSave = 5;
 
-  private int currentLevel = 1;
+    private int currentLevel = 1;
 
-  @Inject
-  private GameObjectContainer gameObjectContainer;
+    @Inject
+    private GameObjectContainer gameObjectContainer;
 
-  @Inject
-  private HighscoreManager highscoreManager;
+    @Inject
+    private HighscoreManager highscoreManager;
 
-  @Inject
-  private TimeManager timeManager;
+    @Inject
+    private TimeManager timeManager;
 
-  @Inject
-  @GameOver
-  private Event<Long> gameOverEvent;
+    @Inject
+    @GameOver
+    private Event<Long> gameOverEvent;
 
-  @Inject
-  @TimeUpdate
-  private Event<TimeData> timeUpdateEvent;
+    @Inject
+    @TimeUpdate
+    private Event<TimeData> timeUpdateEvent;
 
-  @Inject
-  @ScoreUpdate
-  private Event<Long> scoreUpdateEvent;
+    @Inject
+    @ScoreUpdate
+    private Event<Long> scoreUpdateEvent;
 
-  @Inject
-  @LifeUpdate
-  private Event<Long> lifeUpdateEvent;
+    @Inject
+    @LifeUpdate
+    private Event<Long> lifeUpdateEvent;
 
-  private boolean gameOver = false;
+    private boolean gameOver = false;
 
-  @PostConstruct
-  public void initialize() {
-  }
+    @PostConstruct
+    public void initialize() {
+    }
 
-  private Frogger createFroggerAtStartPos() {
-    return Frogger.newInstanceAtStart(
-            new Rectangle(froggerStartPos, froggerSize));
-  }
+    private Frogger createFroggerAtStartPos() {
+        return Frogger.newInstanceAtStart(
+                new Rectangle(froggerStartPos, froggerSize));
+    }
 
-  private Frogger createFroggerInNest(FrogNest nest) {
-    return Frogger.newInstanceInNest(froggerSize, nest);
-  }
+    private Frogger createFroggerInNest(FrogNest nest) {
+        return Frogger.newInstanceInNest(froggerSize, nest);
+    }
 
-  private Streetobject createCar(int streetLvl) {
-    return new Streetobject(new Rectangle(new Vector(-50, streetLvl * 50),
-            new Vector(1, 0), new Vector(50, 25)));
-  }
+    private Streetobject createCar(int streetLvl) {
+        return new Streetobject(new Rectangle(new Vector(-50, streetLvl * 50),
+                new Vector(1, 0), new Vector(50, 25)));
+    }
 
-  private Waterobject createTreeTrunk(int waterLvl) {
-    return new Waterobject(new Rectangle(new Vector(-50, waterLvl * 200),
-            new Vector(2, 0), new Vector(50, 25)));
-  }
+    private Waterobject createTreeTrunk(int waterLvl) {
+        return new Waterobject(new Rectangle(new Vector(-50, waterLvl * 200),
+                new Vector(2, 0), new Vector(50, 25)));
+    }
 
-  private FrogNest createFrogNest(int number) {
-    return new FrogNest(new Rectangle(new Vector(100 * number, 350),
-            new Vector(2, 0), new Vector(75, 75)));
-  }
+    private FrogNest createFrogNest(int number) {
+        return new FrogNest(new Rectangle(new Vector(100 * number, 350),
+                new Vector(2, 0), new Vector(75, 75)));
+    }
 
-  /**
-   * Starts a new game.
-   *  @param frame the frame to start the game on
-   */
-  public KeyListener startGame(Dimension gamePanelSize) {
-    gameObjectContainer.addBorder(gamePanelSize.height, gamePanelSize.width);
+    /**
+     * Starts a new game.
+     *  @param frame the frame to start the game on
+     */
+    public KeyListener startGame(Dimension gamePanelSize) {
+        gameObjectContainer.addBorder(gamePanelSize.height, gamePanelSize.width);
 
-    gameObjectContainer.addFrogger(createFroggerAtStartPos());
+        gameObjectContainer.addFrogger(createFroggerAtStartPos());
 
-    gameObjectContainer.addMobileGameObject(createCar(1));
-    gameObjectContainer.addMobileGameObject(createCar(2));
-    gameObjectContainer.addMobileGameObject(createCar(3));
+        gameObjectContainer.addMobileGameObject(createCar(1));
+        gameObjectContainer.addMobileGameObject(createCar(2));
+        gameObjectContainer.addMobileGameObject(createCar(3));
 
-    gameObjectContainer.addMobileGameObject(createTreeTrunk(1));
+        gameObjectContainer.addMobileGameObject(createTreeTrunk(1));
 
-    gameObjectContainer.addFroggerNest(createFrogNest(1));
-    gameObjectContainer.addFroggerNest(createFrogNest(2));
-    gameObjectContainer.addFroggerNest(createFrogNest(3));
-    gameObjectContainer.addFroggerNest(createFrogNest(4));
-    gameObjectContainer.addFroggerNest(createFrogNest(5));
-    
-    lifeUpdateEvent.fire(remainingLives);
-    scoreUpdateEvent.fire(highscoreManager.getHighScore());
+        gameObjectContainer.addFroggerNest(createFrogNest(1));
+        gameObjectContainer.addFroggerNest(createFrogNest(2));
+        gameObjectContainer.addFroggerNest(createFrogNest(3));
+        gameObjectContainer.addFroggerNest(createFrogNest(4));
+        gameObjectContainer.addFroggerNest(createFrogNest(5));
 
-    timeManager.addActionListener(this);
-    timeManager.startTimer(currentLevel);
-    return this;
-  }
-
-  /**
-   * Ends the current game.
-   */
-  public void endGame() {
-    gameObjectContainer.clear();
-    gameObjectContainer.removeFrogger();
-    remainingLives = 3;
-    remainingFroggersToSave = 5;
-    highscoreManager.resetHighscore();
-    gameOver = false;
-    timeManager.stopTimer();
-  }
-
-  private void prepareNextLevel() {
-    currentLevel++;
-    remainingFroggersToSave = 5;
-    gameObjectContainer.resetFrogger();
-    gameObjectContainer.clearFrogNestInvaders();
-    timeManager.restartTimer(currentLevel);
-  }
-
-  public void listenToFroggerDeath(@Observes FroggerDeath death) {
-    if (!gameOver) {
-      if (--remainingLives > 0) {
-        gameObjectContainer.resetFrogger();
-        timeManager.restartTimer();
         lifeUpdateEvent.fire(remainingLives);
-      } else {
-        gameOver = true;
-        gameOverEvent.fire(highscoreManager.getHighScore());
-      }
+        scoreUpdateEvent.fire(highscoreManager.getHighScore());
+
+        timeManager.startTimer(currentLevel);
+        return this;
     }
-  }
 
-  public void listenToFroggerInNest(@Observes FrogNest nest) {
-    if (--remainingFroggersToSave > 0) {
-      // adds an surviving Frogger to the nest.
-      nest.addInvader(createFroggerInNest(nest));
-      gameObjectContainer.resetFrogger();
-      highscoreManager.updateHighscore(timeManager.getLevelDelay(), timeManager.
-              getElapsedSeconds(), currentLevel);
-      scoreUpdateEvent.fire(highscoreManager.getHighScore());
-      timeManager.restartTimer();
-      System.out.println("Froggers left to save " + remainingFroggersToSave);
-    } else {
-      highscoreManager.updateHighscore(30, 5, currentLevel);
-      prepareNextLevel();
-      System.out.println("Level " + currentLevel);
+    /**
+     * Ends the current game.
+     */
+    public void endGame() {
+        gameObjectContainer.clear();
+        gameObjectContainer.removeFrogger();
+        remainingLives = 3;
+        remainingFroggersToSave = 5;
+        highscoreManager.resetHighscore();
+        gameOver = false;
+        timeManager.stopTimer();
     }
-  }
 
-  public void keyTyped(KeyEvent e) {
-    //ignore event - not triggered by cursor keys
-    e.consume();
-  }
-
-  public void keyPressed(KeyEvent e) {
-    //ignore event - hinder steady movement by holding the key pressed.
-  }
-
-  public void keyReleased(KeyEvent e) {
-    if (!gameOver) {
-      handleKeyInput(e.getKeyCode());
+    private void prepareNextLevel() {
+        currentLevel++;
+        remainingFroggersToSave = 5;
+        gameObjectContainer.resetFrogger();
+        gameObjectContainer.clearFrogNestInvaders();
+        timeManager.restartTimer(currentLevel);
     }
-  }
 
-  private void handleKeyInput(int keyCode) {
-    switch (keyCode) {
-      case RIGHT:
-        gameObjectContainer.moveFroggerRight();
-        break;
-      case LEFT:
-        gameObjectContainer.moveFroggerLeft();
-        break;
-      case UP:
-        gameObjectContainer.moveFroggerUp();
-        break;
-      case DOWN:
-        gameObjectContainer.moveFroggerDown();
-        break;
-      default:
-        System.out.println("Ignore key!");
+    public void listenToFroggerDeath(@Observes FroggerDeath death) {
+        if (!gameOver) {
+            if (--remainingLives > 0) {
+                gameObjectContainer.resetFrogger();
+                timeManager.restartTimer();
+                lifeUpdateEvent.fire(remainingLives);
+            } else {
+                gameOver = true;
+                gameOverEvent.fire(highscoreManager.getHighScore());
+            }
+        }
     }
-  }
 
-  @Override
-  public void actionPerformed(ActionEvent event) {
-    switch (event.getActionCommand()) {
-      case ActionCommand.TIMEUP:
-        listenToFroggerDeath(new FroggerDeath(FroggerDeath.TIMEUP));
-        break;
-      case ActionCommand.TIMEUPDATE:
-        timeUpdateEvent.fire(new TimeData(timeManager.getLevelDelay(),
-                timeManager.getRemainingTime()));
-        break;
+    public void listenToFroggerInNest(@Observes FrogNest nest) {
+        if (--remainingFroggersToSave > 0) {
+            // adds an surviving Frogger to the nest.
+            nest.addInvader(createFroggerInNest(nest));
+            gameObjectContainer.resetFrogger();
+            highscoreManager.updateHighscore(timeManager.getLevelDelay(), timeManager.getElapsedSeconds(), currentLevel);
+            scoreUpdateEvent.fire(highscoreManager.getHighScore());
+            timeManager.restartTimer();
+            System.out.println("Froggers left to save " + remainingFroggersToSave);
+        } else {
+            highscoreManager.updateHighscore(30, 5, currentLevel);
+            prepareNextLevel();
+            System.out.println("Level " + currentLevel);
+        }
     }
-  }
+
+    public void keyTyped(KeyEvent e) {
+        //ignore event - not triggered by cursor keys
+        e.consume();
+    }
+
+    public void keyPressed(KeyEvent e) {
+        //ignore event - hinder steady movement by holding the key pressed.
+    }
+
+    public void keyReleased(KeyEvent e) {
+        if (!gameOver) {
+            handleKeyInput(e.getKeyCode());
+        }
+    }
+
+    private void handleKeyInput(int keyCode) {
+        switch (keyCode) {
+            case RIGHT:
+                gameObjectContainer.moveFroggerRight();
+                break;
+            case LEFT:
+                gameObjectContainer.moveFroggerLeft();
+                break;
+            case UP:
+                gameObjectContainer.moveFroggerUp();
+                break;
+            case DOWN:
+                gameObjectContainer.moveFroggerDown();
+                break;
+            default:
+                System.out.println("Ignore key!");
+        }
+    }
 }
--- Project_Frogger/frogger-reincarnation/src/main/java/org/ks/frogger/manager/TimeManager.java
+++ Project_Frogger/frogger-reincarnation/src/main/java/org/ks/frogger/manager/TimeManager.java
@@ -1,153 +1,165 @@
 package org.ks.frogger.manager;
 
 import com.google.common.base.Stopwatch;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.concurrent.TimeUnit;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Event;
+import javax.inject.Inject;
 import javax.swing.Timer;
 import org.ks.frogger.ActionCommand;
+import org.ks.frogger.events.FroggerDeath;
+import org.ks.frogger.events.TimeData;
+import org.ks.frogger.events.TimeUpdate;
 
 /**
  *
  * @author Kevin Sapper 2011
  */
 @ApplicationScoped
-public class TimeManager {
+public class TimeManager implements ActionListener {
 
-  private final int delay = 30000;
+    private final long delay = 30000;
 
-  private final int minDelay = 5000;
+    private final long minDelay = 5000;
 
-  private int levelDelay = delay;
+    private long levelDelay = delay;
 
-  private Timer timer;
-  
-  private Timer timeUpdateTimer;
+    @Inject
+    @TimeUpdate
+    private Event<TimeData> timeUpdateEvent;
 
-  private Stopwatch stopwatch;
+    @Inject
+    private Event<FroggerDeath> deathEvent;
 
-  @PostConstruct
-  public void initialize() {
-    stopwatch = new Stopwatch();
-    
-    timer = new Timer(delay, null);
-    timer.setActionCommand(ActionCommand.TIMEUP);
-    
-    timeUpdateTimer = new Timer(1000, null);
-    timeUpdateTimer.setInitialDelay(0);
-    timeUpdateTimer.setActionCommand(ActionCommand.TIMEUPDATE);
-  }
+    private Timer timer;
 
-  public void addActionListener(ActionListener actionListener) {
-    boolean addListener = true;
-    for (ActionListener listener : timer.getActionListeners()) {
-      if (listener.equals(actionListener)) {
-        addListener = false;
-      }
+    private Timer timeUpdateTimer;
+
+    private Stopwatch stopwatch;
+
+    @PostConstruct
+    public void initialize() {
+        stopwatch = new Stopwatch();
+
+        timer = new Timer((int) delay, this);
+        timer.setActionCommand(ActionCommand.TIMEUP);
+
+        timeUpdateTimer = new Timer(1000, this);
+        timeUpdateTimer.setInitialDelay(0);
+        timeUpdateTimer.setActionCommand(ActionCommand.TIMEUPDATE);
     }
-    if (addListener) {
-      timer.addActionListener(actionListener);
-      timeUpdateTimer.addActionListener(actionListener);
+
+    /**
+     * 
+     * @param actionListener
+     * @param level 
+     */
+    public void startTimer(int level) {
+        if (!timer.isRunning()) {
+            updateLevelDelay(level);
+
+            timer.setInitialDelay((int) levelDelay);
+            timer.setDelay((int) levelDelay);
+
+            timer.start();
+            timeUpdateTimer.start();
+            stopwatch.start();
+        }
     }
-  }
 
-  /**
-   * 
-   * @param actionListener
-   * @param level 
-   */
-  public void startTimer(int level) {
-    if (!timer.isRunning()) {
-      updateLevelDelay(level);
+    /**
+     * 
+     */
+    public void restartTimer() {
+        if (timer != null && timer.isRunning()) {
+            stopwatch.reset();
 
-      timer.setInitialDelay(levelDelay);
-      timer.setDelay(levelDelay);
-      
-      timer.start();
-      timeUpdateTimer.start();
-      stopwatch.start();
+            timer.restart();
+            timeUpdateTimer.restart();
+
+            stopwatch.start();
+        }
     }
-  }
 
-  /**
-   * 
-   */
-  public void restartTimer() {
-    if (timer != null && timer.isRunning()) {
-      stopwatch.reset();
+    /**
+     * 
+     * @param currentLevel 
+     */
+    public void restartTimer(int currentLevel) {
+        if (timer != null && timer.isRunning()) {
+            stopwatch.reset();
 
-      timer.restart();
-      timeUpdateTimer.restart();
+            updateLevelDelay(currentLevel);
 
-      stopwatch.start();
+            timer.stop();
+            timer.setDelay((int) levelDelay);
+            timer.setInitialDelay((int) levelDelay);
+            timer.start();
+            timeUpdateTimer.restart();
+
+            stopwatch.start();
+        }
     }
-  }
 
-  /**
-   * 
-   * @param currentLevel 
-   */
-  public void restartTimer(int currentLevel) {
-    if (timer != null && timer.isRunning()) {
-      stopwatch.reset();
-
-      updateLevelDelay(currentLevel);
-
-      timer.stop();
-      timer.setDelay(levelDelay);
-      timer.setInitialDelay(levelDelay);
-      timer.start();
-      timeUpdateTimer.restart();
-
-      stopwatch.start();
+    /**
+     * 
+     */
+    public void stopTimer() {
+        if (timer != null && timer.isRunning()) {
+            timer.stop();
+            timeUpdateTimer.stop();
+            stopwatch.stop();
+        }
     }
-  }
 
-  /**
-   * 
-   */
-  public void stopTimer() {
-    if (timer != null && timer.isRunning()) {
-      timer.stop();
-      timeUpdateTimer.stop();
-      stopwatch.stop();
+    /**
+     * Get the elapsed time since timer start
+     * @return the elapsed time in seconds
+     */
+    public long getElapsedSeconds() {
+        return stopwatch.elapsedTime(TimeUnit.SECONDS);
     }
-  }
 
-  /**
-   * Get the elapsed time since timer start
-   * @return the elapsed time in seconds
-   */
-  public long getElapsedSeconds() {
-    return stopwatch.elapsedTime(TimeUnit.SECONDS);
-  }
-
-  /**
-   * Gets the remaining time until the timer trigger again
-   * @return the remaining time in seconds
-   */
-  public long getRemainingTime() {
-    return millisToSeconds(levelDelay) - stopwatch.elapsedTime(TimeUnit.SECONDS);
-  }
-
-  /**
-   * Get the current level trigger delay.
-   * @return level deplay in seconds.
-   */
-  public long getLevelDelay() {
-    return millisToSeconds(levelDelay);
-  }
-
-  private void updateLevelDelay(int level) {
-    if (levelDelay > minDelay) {
-      levelDelay = delay - (level - 1 * 1000);
-    } else {
-      levelDelay = minDelay;
+    /**
+     * Gets the remaining time until the timer trigger again
+     * @return the remaining time in seconds
+     */
+    public long getRemainingTime() {
+        return millisToSeconds(levelDelay) - stopwatch.elapsedTime(TimeUnit.SECONDS);
     }
-  }
-  
-  private long millisToSeconds(long millis) {
-    return millis / 1000;
-  }
+
+    /**
+     * Get the current level trigger delay.
+     * @return level deplay in seconds.
+     */
+    public long getLevelDelay() {
+        return millisToSeconds(levelDelay);
+    }
+
+    private void updateLevelDelay(int level) {
+        if (levelDelay > minDelay) {
+            levelDelay = delay - (level - 1 * 1000);
+        } else {
+            levelDelay = minDelay;
+        }
+    }
+
+    private long millisToSeconds(long millis) {
+        return millis / 1000;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        switch (event.getActionCommand()) {
+            case ActionCommand.TIMEUP:
+                deathEvent.fire(new FroggerDeath(FroggerDeath.TIMEUP));
+                break;
+            case ActionCommand.TIMEUPDATE:
+                timeUpdateEvent.fire(new TimeData(getLevelDelay(),getRemainingTime()));
+                break;
+        }
+    }
 }
