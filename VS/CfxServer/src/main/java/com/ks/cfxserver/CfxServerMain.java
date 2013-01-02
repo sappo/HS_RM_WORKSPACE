73efdd20fc04a3ec7147c71a31d818d0b612954a
sappo <kevinsapper88@gmail.com>
2013-01-02 20:08:23 +0100

    - added private and public key - implemented signing - implemented encrypting

--- VS/CfxServer/src/main/java/com/ks/cfxserver/CfxServerMain.java
+++ VS/CfxServer/src/main/java/com/ks/cfxserver/CfxServerMain.java
@@ -1,9 +1,16 @@
 package com.ks.cfxserver;
 
 import com.ks.cfxinterface.ZKServer;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.cxf.endpoint.Endpoint;
+import org.apache.cxf.endpoint.Server;
 import org.apache.cxf.interceptor.LoggingInInterceptor;
 import org.apache.cxf.interceptor.LoggingOutInterceptor;
 import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
+import org.apache.cxf.ws.security.wss4j.WSS4JInInterceptor;
+import org.apache.cxf.ws.security.wss4j.WSS4JOutInterceptor;
+import org.apache.ws.security.handler.WSHandlerConstants;
 
 /**
  *
@@ -20,6 +27,36 @@ public class CfxServerMain {
         svrFactory.setServiceBean(implementor);
         svrFactory.getInInterceptors().add(new LoggingInInterceptor());
         svrFactory.getOutInterceptors().add(new LoggingOutInterceptor());
-        svrFactory.create();
+        Server server = svrFactory.create();
+
+        Endpoint cxfEndpoint = server.getEndpoint();
+
+        // verify signature and decrypt message
+        Map<String, Object> inProps = new HashMap<>();
+        inProps.put(WSHandlerConstants.ACTION,
+                WSHandlerConstants.TIMESTAMP + " "
+                + WSHandlerConstants.SIGNATURE + " "
+                + WSHandlerConstants.ENCRYPT);
+        inProps.put(WSHandlerConstants.PW_CALLBACK_CLASS, PasswordCallbackHandler.class.getName());
+        inProps.put(WSHandlerConstants.DEC_PROP_FILE, "server.properties");
+        inProps.put(WSHandlerConstants.SIG_PROP_FILE, "server.properties");
+
+        WSS4JInInterceptor wssIn = new WSS4JInInterceptor(inProps);
+        cxfEndpoint.getInInterceptors().add(wssIn);
+
+        // sign and encrypte message
+        Map<String, Object> outProps = new HashMap<>();
+        outProps.put(WSHandlerConstants.ACTION,
+                WSHandlerConstants.TIMESTAMP + " "
+                + WSHandlerConstants.SIGNATURE + " "
+                + WSHandlerConstants.ENCRYPT);
+        outProps.put(WSHandlerConstants.USER, "server");
+        outProps.put(WSHandlerConstants.ENCRYPTION_USER, "useReqSigCert");
+        outProps.put(WSHandlerConstants.PW_CALLBACK_CLASS, PasswordCallbackHandler.class.getName());
+        outProps.put(WSHandlerConstants.ENC_PROP_FILE, "server_sign.properties");
+        outProps.put(WSHandlerConstants.SIG_PROP_FILE, "server_sign.properties");
+
+        WSS4JOutInterceptor wssOut = new WSS4JOutInterceptor(outProps);
+        cxfEndpoint.getOutInterceptors().add(wssOut);
     }
 }
---
+++ VS/CfxServer/src/main/java/com/ks/cfxserver/PasswordCallbackHandler.java
@@ -0,0 +1,25 @@
+package com.ks.cfxserver;
+
+import java.io.IOException;
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import org.apache.ws.security.WSPasswordCallback;
+
+/**
+ *
+ * @author Kevin Sapper (2012)
+ */
+public class PasswordCallbackHandler implements CallbackHandler {
+
+    @Override
+    public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+        WSPasswordCallback pwcb = (WSPasswordCallback) callbacks[0];
+        String id = pwcb.getIdentifier();
+        int usage = pwcb.getUsage();
+        if (usage == WSPasswordCallback.DECRYPT || usage == WSPasswordCallback.SIGNATURE) {
+            // used to retrieve password for private key
+            pwcb.setPassword("server");
+        }
+    }
+}
