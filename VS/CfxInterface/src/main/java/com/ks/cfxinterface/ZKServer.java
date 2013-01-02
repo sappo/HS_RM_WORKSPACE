e950c2cc6424c2d762b1da61700f139b03c49129
sappo <kevinsapper88@gmail.com>
2013-01-02 22:23:40 +0100

    - added namespace for parameter

--- VS/CfxInterface/src/main/java/com/ks/cfxinterface/ZKServer.java
+++ VS/CfxInterface/src/main/java/com/ks/cfxinterface/ZKServer.java
@@ -15,21 +15,21 @@ public interface ZKServer {
 
     @ServerMethod
     @WebMethod
-    @WebResult(name = "result")
-    public String verdoppeln(@WebParam(name = "text") String text, @WebParam(name = "quantity") int quantity);
+    @WebResult(name = "result", partName = "result", targetNamespace = "http://cfxinterface.ks.com/params")
+    public String verdoppeln(@WebParam(name = "text", partName = "text", targetNamespace = "http://cfxinterface.ks.com/params") String text, @WebParam(name = "quantity") int quantity);
 
     @ServerMethod
     @WebMethod
-    @WebResult(name = "result")
-    public String zeichenweisesVerdoppeln(@WebParam(name = "text") String text, @WebParam(name = "quantity") int quantity);
+    @WebResult(name = "result", partName = "result", targetNamespace = "http://cfxinterface.ks.com/params")
+    public String zeichenweisesVerdoppeln(@WebParam(name = "text", partName = "text", targetNamespace = "http://cfxinterface.ks.com/params") String text, @WebParam(name = "quantity") int quantity);
 
     @ServerMethod
     @WebMethod
-    @WebResult(name = "result")
-    public String spiegeln(@WebParam(name = "text") String text);
+    @WebResult(name = "result", partName = "result", targetNamespace = "http://cfxinterface.ks.com/params")
+    public String spiegeln(@WebParam(name = "text", partName = "text", targetNamespace = "http://cfxinterface.ks.com/params") String text);
 
     @ServerMethod
     @WebMethod
-    @WebResult(name = "result")
-    public int laenge(@WebParam(name = "text") String text);
+    @WebResult(name = "result", partName = "result", targetNamespace = "http://cfxinterface.ks.com/params")
+    public int laenge(@WebParam(name = "text", partName = "text", targetNamespace = "http://cfxinterface.ks.com/params") String text);
 }
